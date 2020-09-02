3ddd3e53ee37ef80dfd67115c3945142c52a794c
nkonada <knc@outlook.in>
2020-09-02 19:01:48 +0530

    about page added

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const about = () => {
+  return (
+    <div>
+      <Link to="/">Home Page</Link>
+      This is the about page.
+    </div>
+  )
+}
+
+export default about
